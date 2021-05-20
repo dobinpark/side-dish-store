85d2655763d06b1f24966aefaf901acd310b4389
ff502 <ff5026@naver.com>
2021-05-21 00:07:52 +0900

    Initial commit

---
+++ src/test/java/project1/toy/ToyApplicationTests.java
@@ -0,0 +1,13 @@
+package project1.toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
